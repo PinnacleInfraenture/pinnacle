d26f676ed06a0d6fc1da023d850f32e8a444c785
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-09 23:54:53 +0530

    Add blur on scrollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script>
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
+	let top=true;
+
+	function scrollDetect(){
+		if(window.scrollY<50)
+		top=true;
+		else
+		top=false;
+	}
+	onMount(()=>{document.addEventListener('scroll',scrollDetect);
+return ()=>document.removeEventListener('scroll',scrollDetect)
+})
+
+
 </script>
 
 <svelte:head>
 	<title>Pinnacle</title> 
 </svelte:head>
 
-<div class="fixed top-0 left-0 w-full px-32">
+<div class={`fixed top-0 left-0 w-full z-50 px-32 ${!top? "backdrop-blur-md":""}`}>
 	<Navbar />
 </div>
 <slot />
