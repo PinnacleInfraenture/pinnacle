fa590a4101775a2611af15f1021465e78ec4b50f
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-15 22:33:20 +0530

    Add vite code to get all images

---
+++ src/utils/getGalleryImages.js
@@ -0,0 +1,12 @@
+export async function loadImagePaths() {
+	const imageFiles = import.meta.glob('$lib/assets/ourworks/*.{png,jpg,jpeg,gif,svg,webp}');
+
+	const imagePaths = [];
+	for (const path in imageFiles) {
+		if (imageFiles.hasOwnProperty(path)) {
+			imagePaths.push(path);
+		}
+	}
+
+	return imagePaths;
+}
