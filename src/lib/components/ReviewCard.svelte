7844d91562fbb4f405718288d6c18daebacccda3
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-16 21:20:48 +0530

    Review Card for carousel

---
+++ src/lib/components/ReviewCard.svelte
@@ -0,0 +1,28 @@
+<script>import Star from "$lib/assets/star.png"
+import Cutu from "$lib/assets/person.png"
+</script>
+<div class="">
+	<div class="flex flex-row">
+	<div class="h-2 w-1/6 bg-fadedGolden"></div>
+	<div class="h-2 w-5/6 bg-transparent"></div>
+</div>
+	<div class="bg-white pt-6 px-8">
+		<div class="inline-flex gap-x-3">
+			<img src={Star} alt="" class="w-6 h-6">
+			<img src={Star} alt="" class="w-6 h-6">
+			<img src={Star} alt="" class="w-6 h-6">
+			<img src={Star} alt="" class="w-6 h-6">
+			<img src={Star} alt="" class="w-6 h-6">
+
+		</div>
+		<p class="text-2xl py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum illum odio vitae porro, voluptas laborum consequatur possimus rem voluptates?</p>
+	</div>
+		<div class="bg-fadedGolden px-8 py-5 inline-flex gap-x-5 items-center w-full">
+			<img src={Cutu} class="w-20 h-20" alt="Person">
+<div class="">
+	<h3 class="font-extrabold text-white text-xl">Some Girl</h3>
+	<p class="text-[#E2E2E2] text-md">Engineer</p>
+
+</div>
+		</div>
+</div>
