84eb571e5ffd0ce15484d55bca48452dcaf24b1f
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-21 00:15:17 +0530

    Work on Responsiveness

--- src/lib/components/ReviewCard.svelte
+++ src/lib/components/ReviewCard.svelte
@@ -6,23 +6,22 @@ import Cutu from "$lib/assets/person.png"
 	<div class="h-2 w-1/6 bg-fadedGolden"></div>
 	<div class="h-2 w-5/6 bg-transparent"></div>
 </div>
-	<div class="bg-white pt-6 px-8">
+	<div class="bg-white pt-6 px-3 md:px-8">
 		<div class="inline-flex gap-x-3">
-			<img src={Star} alt="" class="w-6 h-6">
-			<img src={Star} alt="" class="w-6 h-6">
-			<img src={Star} alt="" class="w-6 h-6">
-			<img src={Star} alt="" class="w-6 h-6">
-			<img src={Star} alt="" class="w-6 h-6">
+			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
+			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
+			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
+			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
+			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
 
 		</div>
-		<p class="text-2xl py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum illum odio vitae porro, voluptas laborum consequatur possimus rem voluptates?</p>
+		<p class="text-[9.5px] md:text-2xl py-4 md:py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum illum odio vitae porro, voluptas laborum consequatur possimus rem voluptates?</p>
 	</div>
-		<div class="bg-fadedGolden px-8 py-5 inline-flex gap-x-5 items-center w-full">
-			<img src={Cutu} class="w-20 h-20" alt="Person">
+		<div class="bg-fadedGolden px-3 md:px-8 py-3 md:py-5 inline-flex gap-x-5 items-center w-full">
+			<img src={Cutu} class="md:w-20 md:h-20 w-10 h-10" alt="Person">
 <div class="">
-	<h3 class="font-extrabold text-white text-xl">Some Girl</h3>
-	<p class="text-[#E2E2E2] text-md">Engineer</p>
-
+	<h3 class="font-extrabold text-white text-[8px] md:text-xl">Some Girl</h3>
+	<p class="text-[#E2E2E2] text-[5px] md:text-md">Engineer</p>
 </div>
 		</div>
 </div>
