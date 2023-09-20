a1a481f03ac737c5ffdf3dabda05654a03364b19
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-21 00:13:53 +0530

    Work on Responsiveness

--- src/lib/components/AboutUs.svelte
+++ src/lib/components/AboutUs.svelte
@@ -6,15 +6,15 @@
 
 
 </script>
- <IntersectionObserver id="aboutus" classNames="w-full bg-greyBG py-32 relative px-32 flex md:flex-row gap-x-10">
+ <IntersectionObserver id="aboutus" classNames="w-full bg-greyBG py-32 relative px-10 gap-y-5 md:px-32 flex flex-col md:flex-row gap-x-10">
 	<img src={YellowLines} alt="" class="absolute top-5 left-5">
-	<div class="md:w-[55%]">
+	<div class="w-full md:w-[55%]">
 		<SectionTitle title="About Us" />
-		<h4 class="font-medium text-white mt-14 text-2xl">Pinnacle is a rapidly growing, innovative engineering and construction company. With over 10+ years of experience in delivering exceptionally high standard construction projects, specializing in providing a comprehensive build and design, construction management, facility management and infrastructure services.</h4>
-		<button class="bg-fadedGolden mt-10 text-2xl border-2 border-fadedGolden px-8 py-2 rounded-md text-white font-casta">Learn More</button>
+		<h4 class="font-medium text-white mt-14 text-md md:text-2xl">Pinnacle is a rapidly growing, innovative engineering and construction company. With over 10+ years of experience in delivering exceptionally high standard construction projects, specializing in providing a comprehensive build and design, construction management, facility management and infrastructure services.</h4>
+		<button class="bg-fadedGolden mt-10 text-md md:text-2xl border-2 border-fadedGolden px-8 py-2 rounded-md text-white font-casta">Learn More</button>
 
 	</div>
-	<div class="md:w-[45%]">
+	<div class="w-full md:w-[45%]">
 		<img src={AboutUs} alt="About Us">
 	</div>
  </IntersectionObserver>
