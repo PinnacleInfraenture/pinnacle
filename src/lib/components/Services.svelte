88052f9616a195508fb23535df24d5f3cc464f01
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-21 00:15:37 +0530

    Work on Responsiveness

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -8,10 +8,10 @@
 	import HoverServices from "./HoverServices.svelte";
 
 </script>
-<IntersectionObserver id="services" classNames="w-full bg-greyBG py-32 relative px-32">
+<IntersectionObserver id="services" classNames="w-full bg-greyBG py-32 relative px-10 md:px-32">
 	<img src={YellowLines} alt="" class="absolute top-5 right-5">
 	<SectionTitle title="Services" />
-	<h4 class="font-medium text-white my-14 text-2xl">Our ability to deliver quality services has grown over the years because of experienced workforce.</h4> <div class="grid grid-cols-1 md:grid-cols-3 md:gap-x-12">
+	<h4 class="font-medium text-white my-14 text-md md:text-2xl">Our ability to deliver quality services has grown over the years because of experienced workforce.</h4> <div class="grid grid-cols-1 md:grid-cols-3 max-md:gap-y-10 md:gap-x-12">
 		<HoverServices src={HomeConstruction} alt="Home Construction" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
 		<HoverServices src={Planning} alt="Planning" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
 		<HoverServices src={Renovation} alt="Renovation" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
