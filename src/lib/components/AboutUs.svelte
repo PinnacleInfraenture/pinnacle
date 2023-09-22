7cd4392d33613af7789d16d661a9f12b8fc49082
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-22 14:44:23 +0530

    Work on Responsiveness

--- src/lib/components/AboutUs.svelte
+++ src/lib/components/AboutUs.svelte
@@ -9,7 +9,9 @@
  <IntersectionObserver id="aboutus" classNames="w-full bg-greyBG py-32 relative px-10 gap-y-5 md:px-32 flex flex-col md:flex-row gap-x-10">
 	<img src={YellowLines} alt="" class="absolute top-5 left-5">
 	<div class="w-full md:w-[55%]">
+		<div class="max-md:grid max-md:place-items-center">
 		<SectionTitle title="About Us" />
+			</div>
 		<h4 class="font-medium text-white mt-14 text-md md:text-2xl">Pinnacle is a rapidly growing, innovative engineering and construction company. With over 10+ years of experience in delivering exceptionally high standard construction projects, specializing in providing a comprehensive build and design, construction management, facility management and infrastructure services.</h4>
 		<button class="bg-fadedGolden mt-10 text-md md:text-2xl border-2 border-fadedGolden px-8 py-2 rounded-md text-white font-casta">Learn More</button>
 
