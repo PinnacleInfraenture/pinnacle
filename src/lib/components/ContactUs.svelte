d7a3785f21a218079d7d1d540183d4176ca64cf4
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-22 14:44:27 +0530

    Work on Responsiveness

--- src/lib/components/ContactUs.svelte
+++ src/lib/components/ContactUs.svelte
@@ -13,13 +13,19 @@
 	</div>
 	<img src={YellowLines} alt="" class="absolute -top-40 right-5">
 	<div class="flex h-full min-h-full  flex-col w-full md:w-[60%] justify-between">
+				<div class="max-md:grid max-md:place-items-center">
 		<SectionTitle title="Contact Us" />
+			</div>
 		<h4 class="text-white font-medium text-md max-md:text-center md:text-2xl mt-8">Any queries? Feel free to reach us.</h4>
 		<form action="" class="flex flex-col gap-y-10 mt-10">
 			<input class="px-10 py-4" placeholder="Name*">
 			<input  class="px-10 py-4" placeholder="Email*">
 			<textarea rows='8' class="px-10 py-4" placeholder="Message*" />
-			<button type="submit" class="font-casta text-2xl px-6 w-fit py-3 rounded-lg border-2 border-fadedGolden text-fadedGolden">Get in Touch</button>
+			
+
+				<div class="max-md:grid max-md:place-items-center">
+			<button type="submit" class="font-casta text-md md:text-2xl px-6 w-fit py-3 rounded-lg border-2 border-fadedGolden text-fadedGolden">Get in Touch</button>
+				</div>
 		</form>
 	</div>
 </IntersectionObserver>
