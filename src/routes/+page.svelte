08a7ab018b8875ad87e1888aae4698004931bfcf
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-27 00:00:56 +0530

    Move home to new page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,13 @@
 <script>
-	import IntersectionObserver from "$lib/components/IntersectionObserver.svelte";
 	import Section100Img from "$lib/assets/100section.png"
 	import Services from "$lib/components/Services.svelte";
 	import ContactUs from "$lib/components/ContactUs.svelte";
 	import AboutUs from "$lib/components/AboutUs.svelte";
 	import OurWorks from "$lib/components/OurWorks.svelte";
 	import Testimonials from "$lib/components/Testimonials.svelte";
+	import Home from "$lib/components/Home.svelte";
 </script>
-<IntersectionObserver id="home" classNames="">
-	<div class="h-screen w-full bg-black/40 bg-cover animateBG bg-no-repeat grid place-items-center max-md:px-10">
-		<div class="">
-			<h1 class="capitalize text-white font-casta text-[42px] md:text-[100px]">
-				We redefine your
-			</h1>
-			<div class="h-[48px] md:h-[120px] overflow-hidden">
-				<div class="font-casta text-fadedGolden animateText text-[42px] md:text-[100px]">
-					<h2>Dream.</h2>
-					<h2>Desire.</h2>
-					<h2>Future.</h2>
-				</div>
-			</div>
-			<h3 class="text-white text-base md:text-2xl py-5 md:py-10 font-medium">Dreams are meant to come true, We’ll make it happen for you</h3>
-			<div class="inline-flex gap-x-4">
-				<button class="bg-fadedGolden text-md md:text-2xl border-2 border-fadedGolden px-5 md:px-8 py-2 rounded-md text-white font-casta">Our Works</button>
-				<button class="bg-transparent text-md md:text-2xl border-fadedGolden border-2 px-5 md:px-8 py-2 rounded-md text-fadedGolden font-casta">Contact Us</button>
-			</div>
-		</div>
-	</div>
-</IntersectionObserver>
+	<Home />
 <AboutUs />
 <div class="relative">
 	<img src={Section100Img} alt="100%">
@@ -57,48 +37,3 @@
 <OurWorks />
 <Testimonials />
 <ContactUs />
-<style>
-	@keyframes textScroll{
-	0%{
-		transform:translateY(0%);
-	}
-	10%{transform:translateY(0%)}
-	40%{transform:translateY(-33%)}
-	50%{transform:translateY(-33%)}
-	80%{transform:translateY(-66%)}
-	90%{transform:translateY(-66%)}
-	100%{transform:translateY(0%)}
-	}
-		@keyframes bgAnimate{
-		0%{
-			background-image: url('$lib/assets/bg-1.webp');
-		}
-		10%{
-		background-image: url('$lib/assets/bg-1.webp');
-	}
-	40%{
-	background-image: url('$lib/assets/bg-2.webp');
-	}
-	50%{
-		background-image: url('$lib/assets/bg-2.webp');
-	}
-	80%{
-	background-image: url('$lib/assets/bg-3.webp');
-	}
-	90%{
-		background-image: url('$lib/assets/bg-3.webp');
-	}
-	100%{
-	background-image: url('$lib/assets/bg-1.webp');
-	}
-	}
-	.animateText{
-		transform:translateY(-33%);
-		animation: textScroll 5s ease-in-out infinite forwards;
-		}
-		.animateBG{
-			transition: background-image 1s ease-in-out;
-			animation:bgAnimate 5s ease-in-out infinite forwards
-
-	}
-</style>
