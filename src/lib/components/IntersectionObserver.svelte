01fb2e703c673f1a9353d23be3872ca697539123
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-27 00:00:44 +0530

    Rewrite and fix

--- src/lib/components/IntersectionObserver.svelte
+++ src/lib/components/IntersectionObserver.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
-	import { inView } from "../../stores/inViewStore";
+	import { inView as inViewStore } from "../../stores/inViewStore";
 	export let id:string;
 	import {inview} from "svelte-inview";
 	export let classNames:string;
+	let iv=false;
 </script>
 
 <div use:inview={{
 	rootMargin: '0px',
-	threshold: 1,
+	threshold: 0.8,
 	root: null,
 }} 
-on:inview_enter={()=>{
-			inView.set("#"+id);
-}}
- id={id} class={classNames}>
-	<slot></slot>
+	on:inview_enter={event=>{
+		inViewStore.set(id);
+
+	}}>
+	<div
+		use:inview={{
+			rootMargin: '0px',
+			threshold: 0.3,
+			root: null,
+		}} 
+		on:inview_enter={evt=>{
+			iv=true;
+		}}
+			on:inview_leave={evt=>{
+			iv=false;
+
+	}}
+
+	id={id} class={`${classNames} transition-[blur,opacity] duration-1000 ease-in-out ${iv ? 'opacity-100 blur-0' : 'blur-sm opacity-20'}`}>
+		<slot />
+	</div>
 </div>
 
