e482d2ae4ba8e6cc64684350a4c8bfd86aed505c
Gopal Kaul <gopalkaul5@gmail.com>
2023-09-27 00:00:38 +0530

    Remove #

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -20,48 +20,48 @@ $: console.log($inView)
 	{#if hamburger}
 		<ul in:fade out:fly class="w-full md:hidden absolute backdrop-blur-md py-2 top-full items-end right-0 flex flex-col gap-y-2">
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#home" class={`${$inView==="#home"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Home</a>
-			<div class={`${$inView==="#home"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#home" class={`${$inView==="home"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Home</a>
+			<div class={`${$inView==="#home"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#aboutus" class={`${$inView==="#aboutus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>About Us</a>
-			<div class={`${$inView==="#aboutus"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#aboutus" class={`${$inView==="aboutus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>About Us</a>
+			<div class={`${$inView==="aboutus"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#services" class={`${$inView==="#services"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Services</a>
-			<div class={`${$inView==="#services"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#services" class={`${$inView==="services"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Services</a>
+			<div class={`${$inView==="services"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#ourworks" class={`${$inView==="#ourworks"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Our Works</a>
-			<div class={`${$inView==="#ourworks"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#ourworks" class={`${$inView==="ourworks"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Our Works</a>
+			<div class={`${$inView==="ourworks"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#contactus" class={`${$inView==="#contactus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Contact Us</a>
-			<div class={`${$inView==="#contactus"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#contactus" class={`${$inView==="contactus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Contact Us</a>
+			<div class={`${$inView==="contactus"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		</ul>
 	{/if}
 
 	<ul class="max-md:hidden inline-flex items-center gap-x-7 h-full min-h-full font-casta font-medium">
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#home" class={`${$inView==="#home"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Home</a>
-			<div class={`${$inView==="#home"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#home" class={`${$inView==="home"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Home</a>
+			<div class={`${$inView==="home"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#aboutus" class={`${$inView==="#aboutus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>About Us</a>
-			<div class={`${$inView==="#aboutus"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#aboutus" class={`${$inView==="aboutus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>About Us</a>
+			<div class={`${$inView==="aboutus"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#services" class={`${$inView==="#services"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Services</a>
-			<div class={`${$inView==="#services"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#services" class={`${$inView==="services"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Services</a>
+			<div class={`${$inView==="services"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#ourworks" class={`${$inView==="#ourworks"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Our Works</a>
-			<div class={`${$inView==="#ourworks"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#ourworks" class={`${$inView==="ourworks"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Our Works</a>
+			<div class={`${$inView==="ourworks"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 		<li class="relative h-full text-center grid place-items-center">
-			<a href="#contactus" class={`${$inView==="#contactus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Contact Us</a>
-			<div class={`${$inView==="#contactus"?"opacity-1" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
+			<a href="#contactus" class={`${$inView==="contactus"?"text-fadedGolden":"text-white"} transition-colors duration-500 ease-in-out`}>Contact Us</a>
+			<div class={`${$inView==="contactus"?"opacity-100" : "opacity-0"} absolute bottom-0 left-0 rounded-t-xl bg-fadedGolden w-full h-1 transition-all duration-500 ease-in-out`}></div>
 		</li>
 	</ul>
 </nav>
