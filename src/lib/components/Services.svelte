ad218612539ed1fe553b92608580400de1d90eda
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-22 21:03:41 +0530

    Change services

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -15,9 +15,10 @@
 	<SectionTitle title="Services" />
 		</div>
 	<h4 class="font-medium text-white my-14 text-md md:text-2xl">Our ability to deliver quality services has grown over the years because of experienced workforce.</h4> <div class="grid grid-cols-1 md:grid-cols-3 max-md:gap-y-10 md:gap-x-12">
-		<HoverServices src={HomeConstruction} alt="Home Construction" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-		<HoverServices src={Planning} alt="Planning" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
-		<HoverServices src={Renovation} alt="Renovation" hoverText="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." />
+		<HoverServices src={HomeConstruction} alt="Construction Contractor" hoverText="As reputable home construction contractors, we understand the importance of delivering top-notch quality in every project. We deliver superior house construction, backed by a deep understanding of the local housing market and a team of experienced house construction contractors." />
+		<HoverServices src={Planning} alt="Precision Planning" hoverText="Count on Pinnacle Infraventure for unparalleled expertise in planning your dream home. As leading building contractors in Bangalore, we tailor our services to your unique vision, offering accurate house construction cost, transparent house building estimates, and meticulous planning to make your dream a reality." />
+		<HoverServices src={Renovation} alt="Renovation" hoverText="Looking to refresh your living space? Our team of experienced home renovation contractors is equipped to provide top-notch renovation services. Our seasoned team understands the dynamics of house renovation in Bangalore. With meticulous planning and execution, we breathe new life into your cherished space.
+" />
 	</div>
 </IntersectionObserver>
 
