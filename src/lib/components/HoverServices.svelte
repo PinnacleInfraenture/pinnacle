12c4849d1b4c8e61fa12e79bb8ec59dce7f531ca
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-22 22:39:43 +0530

    Fix responsiveness

--- src/lib/components/HoverServices.svelte
+++ src/lib/components/HoverServices.svelte
@@ -2,13 +2,20 @@
 	export let src='';
 	export let alt='';
 	export let hoverText="";
+	export let showText=false;
 </script>
 
-<div class="group relative">
-	<img src={src} alt={alt} class="w-full opacity-1 group-hover:opacity-0 transition-all duration-500 ease-in-out h-full object-contain">
+<div class="group relative" on:click={()=>{
+	if(window.innerWidth<=768){
+		showText=!showText;
+	}
+}}
+
+>
+	<img src={src} alt={alt} class={`w-full group-hover:opacity-0 transition-all duration-500 ease-in-out h-full object-contain ${showText&&'opacity-0'}`}>
 	<div 
 		style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.16) -88.12%, rgba(0, 0, 0, 0.8) 100%);"
-		class="absolute z-50 top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 transition-all duration-500 grid place-items-center p-8 ease-in-out text-white text-2xl">
+		class={`absolute z-50 top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 transition-all duration-500 grid place-items-center p-8 ease-in-out text-white text-md md:text-2xl ${showText && 'opacity-100' }`}>
 		<p>{hoverText}</p>
 	</div>
 </div>
