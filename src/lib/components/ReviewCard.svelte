b427e1227a2252e25d15309638e9b266f2e955ae
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-23 14:26:29 +0530

    Add props

--- src/lib/components/ReviewCard.svelte
+++ src/lib/components/ReviewCard.svelte
@@ -1,5 +1,7 @@
 <script>import Star from "$lib/assets/star.png"
 import Cutu from "$lib/assets/person.png"
+export let text='';
+export let name='';
 </script>
 <div class="">
 	<div class="flex flex-row">
@@ -15,13 +17,11 @@ import Cutu from "$lib/assets/person.png"
 			<img src={Star} alt="" class="w-4 md:w-6 h-4 md:h-6">
 
 		</div>
-		<p class="text-[9.5px] md:text-2xl py-4 md:py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum illum odio vitae porro, voluptas laborum consequatur possimus rem voluptates?</p>
+		<p class="text-[9.5px] md:text-2xl py-4 md:py-8">{text}</p>
 	</div>
 		<div class="bg-fadedGolden px-3 md:px-8 py-3 md:py-5 inline-flex gap-x-5 items-center w-full">
-			<img src={Cutu} class="md:w-20 md:h-20 w-10 h-10" alt="Person">
 <div class="">
-	<h3 class="font-extrabold text-white text-[8px] md:text-xl">Some Girl</h3>
-	<p class="text-[#E2E2E2] text-[5px] md:text-md">Engineer</p>
+	<h3 class="font-extrabold text-white text-[8px] md:text-xl">{name}</h3>
 </div>
 		</div>
 </div>
