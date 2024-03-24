bb4c409896d21106647bbd93ff48699235dae9d1
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-24 23:42:19 +0530

    Add about us intersection

--- src/lib/components/AboutUs.svelte
+++ src/lib/components/AboutUs.svelte
@@ -3,21 +3,37 @@
 	import AboutUs from "$lib/assets/aboutus.png";
 	import SectionTitle from "$lib/components/SectionTitle.svelte";
 	import IntersectionObserver from "./IntersectionObserver.svelte";
+	import {inview} from "svelte-inview";
 
-
+	let intersection=false;
 </script>
- <IntersectionObserver id="aboutus" classNames="w-full bg-greyBG py-32 relative px-10 gap-y-5 md:px-32 flex flex-col md:flex-row gap-x-10">
-	<img src={YellowLines} alt="" class="absolute top-5 left-5">
-	<div class="w-full md:w-[55%]">
-		<div class="max-md:grid max-md:place-items-center">
-		<SectionTitle title="About Us" />
-			</div>
-		<h4 class="font-medium text-white mt-14 text-md md:text-2xl">At Pinnacle Infraventure, we are proud to be recognized among the top builders in Bangalore. With more than a decade of experience in home construction, our legacy in the city is a testament to our commitment to excellence. Our expertise goes beyond just house construction; we have established ourselves as a trusted name among home builders in Bangalore. 
-</h4>
-		<button class="bg-fadedGolden mt-10 text-md md:text-2xl border-2 border-fadedGolden px-8 py-2 rounded-md text-white font-casta">Learn More</button>
+<div class=""
+	use:inview={{
+		threshold:0.6,
+	}}
+	on:inview_enter={()=>{
+		intersection=true;
+	}}
+	on:inview_leave={()=>{
+		intersection=false;
+	}}
 
-	</div>
-	<div class="w-full md:w-[45%]">
-		<img src={AboutUs} alt="About Us">
-	</div>
- </IntersectionObserver>
+	>
+	<IntersectionObserver 
+		id="aboutus" classNames="w-full bg-greyBG py-32 relative px-10 gap-y-5 md:px-32 flex flex-col md:flex-row gap-x-10">
+		<img src={YellowLines} alt="" class="absolute top-5 left-5">
+		<div 
+			class="w-full md:w-[55%]">
+			<div class="max-md:grid max-md:place-items-center">
+				<SectionTitle show={intersection} title="About Us" />
+			</div>
+			<h4 class={`font-medium text-white mt-14 text-md md:text-2xl ${intersection?'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition-[opacity,transform] duration-700 delay-300 ease-in-out`}>At Pinnacle Infraventure, we are proud to be recognized among the top builders in Bangalore. With more than a decade of experience in home construction, our legacy in the city is a testament to our commitment to excellence. Our expertise goes beyond just house construction; we have established ourselves as a trusted name among home builders in Bangalore. 
+			</h4>
+			<button class="bg-fadedGolden mt-10 text-md md:text-2xl border-2 border-fadedGolden px-8 py-2 rounded-md text-white font-casta">Learn More</button>
+
+		</div>
+		<div class="w-full md:w-[45%]">
+			<img src={AboutUs} alt="About Us">
+		</div>
+	</IntersectionObserver>
+</div>
