aa38a196951658afc025f904911450d0d05d3c21
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-24 23:42:44 +0530

    Animate in view

--- src/lib/components/SectionTitle.svelte
+++ src/lib/components/SectionTitle.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
 	export let title:string;
+	export let show:boolean=true;
 </script>
-<h3 data-text={title} class="font-semibold sectionTitle font-casta text-4xl relative">{title}</h3>
+<h3 data-text={title} class={`font-semibold sectionTitle font-casta text-4xl relative ${show?'opacity-100':'opacity-0'} transition-opacity duration-500 ease-in-out`}>{title}</h3>
 
 <style>
 .sectionTitle{
-	font-weight: 600;
-	font-family: "Casta";
-	color:transparent;
-      	-webkit-text-stroke: 1px #D8C691;
-      	font-size:60px;
-	width:fit-content;
+font-weight: 600;
+font-family: "Casta";
+color:transparent;
+-webkit-text-stroke: 1px #D8C691;
+font-size:60px;
+width:fit-content;
 }
 
 .sectionTitle:after{
-	background: linear-gradient(to top, #333333,#333333,transparent);
-	position:absolute;
-	width:100%;
-	top:15px;
-	left:0;
-     	text-align: center;
-     	font-size:50px;
-     	font-weight:600;
-	color:#D8C691;
-	content:attr(data-text);
+background: linear-gradient(to top, #333333,#333333,transparent);
+position:absolute;
+width:100%;
+top:15px;
+left:0;
+text-align: center;
+font-size:50px;
+font-weight:600;
+color:#D8C691;
+content:attr(data-text);
 }
 @media only screen and (max-width: 600px) {
 	.sectionTitle{
 		font-size:30px;
 	}
 	.sectionTitle:after{
-				font-size:25px;
+		font-size:25px;
 
 	}
 }
