94f210b700269e9246da154c8e8a0d7ce5a4aba5
Gopal Kaul <gopalkaul5@gmail.com>
2024-03-24 23:42:41 +0530

    Animate inView

--- src/lib/components/OurWorks.svelte
+++ src/lib/components/OurWorks.svelte
@@ -30,47 +30,61 @@
 		return rotatedArray;
 	}
 
+	let inView=false;
+	import {inview} from "svelte-inview"
 </script>
 
-<IntersectionObserver id="portfolio" classNames="w-full bg-greyBG py-10 relative px-10 md:px-32">
-	<img src={YellowDots} alt="Yellow Dots" class="absolute top-0 right-0">
-				<div class="max-md:grid max-md:place-items-center">
-	<SectionTitle title="Portfolio" />
-		</div>
-	<h4 class="font-medium text-md md:text-2xl text-white my-5 md:my-20">Our extensive portfolio of successful projects showcases our ability to bring your dream home to life.</h4>
-	<div class="grid grid-cols-1 md:grid-cols-2 gap-x-10 mt-10 gap-y-5 relative md:h-[80vh]">
-		<button on:click={
-			()=>imagePaths=rotateArray(imagePaths,-5)
-			} class="max-md:hidden absolute top-1/2 bottom-1/2 -left-20">
-			<img src={ArrowRight} alt="" class="h-16 rotate-180">
-		</button>
-		<button on:click={
-			()=>imagePaths=rotateArray(imagePaths,5)
-			} class="max-md:hidden absolute top-1/2 bottom-1/2 -right-20">
-			<img src={ArrowRight} alt="" class="h-16">
-		</button>
-		<div class="w-full h-full min-h-full">
-			<img src={imagePaths[0]} alt="Portfolio" class="w-full h-full object-cover z-30" />
-		</div>
-		<div class="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-5">
-			<div class="w-full h-full aspect-square">
-				<img src={imagePaths[1]} alt="" class="w-full h-full aspect-square object-cover z-30">
-			</div>
-			<div class="w-full h-full aspect-square">
-				<img src={imagePaths[2]} alt="" class="w-full h-full aspect-square object-cover z-30">
-			</div>
-			<div class="w-full h-full aspect-square">
-				<img src={imagePaths[3]} alt="" class="w-full h-full aspect-square object-cover z-30">
-			</div>
-			<div class="w-full h-full aspect-square">
-				<img src={imagePaths[4]} alt="" class="w-full h-full aspect-square object-cover z-30">
-			</div>
-		</div>
-	</div>
-	<div class="md:hidden grid place-items-center mt-4">
-					<button on:click={()=>{
-						imagePaths=rotateArray(imagePaths,5)
-					}} class="font-casta text-md px-6 w-fit py-3 rounded-lg border-2 border-fadedGolden z-30 text-fadedGolden">Load More</button>
+<div class=""
+	use:inview={{
+		threshold:0.3,
+	}}
+	on:inview_enter={()=>{
+		inView=true;
+	}}
+	on:inview_leave={()=>{
+		inView=false;
+	}}>
 
-	</div>
-</IntersectionObserver>
+	<IntersectionObserver id="portfolio"classNames="w-full bg-greyBG py-10 relative px-10 md:px-32">
+		<img src={YellowDots} alt="Yellow Dots" class="absolute top-0 right-0">
+		<div class="max-md:grid max-md:place-items-center">
+			<SectionTitle show={inView} title="Portfolio" />
+		</div>
+		<h4 class={`font-medium text-md md:text-2xl text-white my-5 md:my-20 ${inView?'opacity-100 translate-y-0':'translate-y-10 opacity-0'} delay-300 transition-[opacity,transform] duration-700 ease-in-out`}>Our extensive portfolio of successful projects showcases our ability to bring your dream home to life.</h4>
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-x-10 mt-10 gap-y-5 relative md:h-[80vh]">
+			<button on:click={
+				()=>imagePaths=rotateArray(imagePaths,-5)
+				} class="max-md:hidden absolute top-1/2 bottom-1/2 -left-20">
+				<img src={ArrowRight} alt="" class="h-16 rotate-180">
+			</button>
+			<button on:click={
+				()=>imagePaths=rotateArray(imagePaths,5)
+				} class="max-md:hidden absolute top-1/2 bottom-1/2 -right-20">
+				<img src={ArrowRight} alt="" class="h-16">
+			</button>
+			<div class="w-full h-full min-h-full">
+				<img src={imagePaths[0]} alt="Portfolio" class="w-full h-full object-cover z-30" />
+			</div>
+			<div class="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-5">
+				<div class="w-full h-full aspect-square">
+					<img src={imagePaths[1]} alt="" class="w-full h-full aspect-square object-cover z-30">
+				</div>
+				<div class="w-full h-full aspect-square">
+					<img src={imagePaths[2]} alt="" class="w-full h-full aspect-square object-cover z-30">
+				</div>
+				<div class="w-full h-full aspect-square">
+					<img src={imagePaths[3]} alt="" class="w-full h-full aspect-square object-cover z-30">
+				</div>
+				<div class="w-full h-full aspect-square">
+					<img src={imagePaths[4]} alt="" class="w-full h-full aspect-square object-cover z-30">
+				</div>
+			</div>
+		</div>
+		<div class="md:hidden grid place-items-center mt-4">
+			<button on:click={()=>{
+				imagePaths=rotateArray(imagePaths,5)
+			}} class="font-casta text-md px-6 w-fit py-3 rounded-lg border-2 border-fadedGolden z-30 text-fadedGolden">Load More</button>
+
+		</div>
+	</IntersectionObserver>
+</div>
